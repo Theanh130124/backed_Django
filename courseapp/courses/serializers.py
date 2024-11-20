f60523377b9c135af515caab52c66765107c630f
Thế Anh <163394900+Theanh130124@users.noreply.github.com>
2024-11-21 01:11:17 +0700

    Update serializers.py

--- courseapp/courses/serializers.py
+++ courseapp/courses/serializers.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id','name']
 
 
 class TagSerializer(serializers.ModelSerializer):
