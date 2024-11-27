6c1d7bbbac68eb9beadc1c4aa4c5f0b03db3bdb9
Thế Anh <163394900+Theanh130124@users.noreply.github.com>
2024-11-27 21:15:42 +0700

    Update perms.py

--- courseapp/courses/perms.py
+++ courseapp/courses/perms.py
@@ -2,6 +2,8 @@
 from rest_framework import permissions
 
 class OwnerAuthenticated(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, obj):
-        #Chỉ có tài khoản comment mới đc xóa comment của chính cái tài khoản đó
-        return self.has_permission(request, view) and request.user == obj.user
+    # def has_object_permission(self, request, view, obj):
+    #     #Chỉ có tài khoản comment mới đc xóa comment của chính cái tài khoản đó
+    #     return self.has_permission(request, view) and request.user == obj.user
+    def has_object_permission(self, request, view, comment):
+        return super().has_permission(request, view) and request.user == comment.user
